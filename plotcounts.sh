dee63bd9c478f5374c7138c6977dd48a90eb2b1e
Dave Vieglais <dave.vieglais@gmail.com>
2012-07-18 16:00:49 +0000

    Generalized script a little to work with previous n (30) days from now.

--- plotcounts.sh
+++ plotcounts.sh
@@ -4,8 +4,9 @@
 # Author: Matt Jones, NCEAS, UC Santa Barbara
 
 CURL="curl -g -k -s"
-XML=xmlstarlet
+XML=xml
 DFILE="plotcounts.csv"
+NDAYS=30
 
 function main {
     buildDataFile
@@ -15,8 +16,8 @@ function main {
 # Iterate over 30 days and get a package count for each day
 function buildDataFile {
     echo "day | count" > $DFILE
-    for d in {01..30}; do 
-        D="2012-06-${d}"
+    for (( d=0; d<=$NDAYS; d++ )); do 
+        D=$(expr $NDAYS - $d)
         count $D
     done
 }
@@ -28,8 +29,9 @@ function generatePlot {
 # Function to retrieve a count of science metadata on the CNs up to a given date
 function count {
     DAY=$1
+    DATE=$(date -v "-${DAY}d" +%Y-%m-%d)
     TIME="T23:59:59.999Z/DAY%2B1DAYS"
-    DM="*%20TO%20${DAY}$TIME"
+    DM="*%20TO%20NOW-${DAY}DAY"
     MOD="dateModified:[${DM}]"
     FORM="%20AND%20NOT%20formatId:%22http://www.openarchives.org/ore/terms%22"
     ROW="&rows=0"
@@ -37,7 +39,8 @@ function count {
     URI=${BASE}${MOD}${FORM}${ROW}
     
     CNT=`$CURL ${URI} | $XML sel -N d1=http://ns.dataone.org/service/types/v1 -t -v /d1:objectList/@total`
-    echo "$DAY | $CNT" >> $DFILE
+    echo "$DATE, $CNT"
+    echo "$DATE | $CNT" >> $DFILE
 }
 # execute the main method
 main
